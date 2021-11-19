ce0c9bdecc560bf6a0f976c307e287e4bb6c4eee
Jannis_Gufa <jannis.kitanovski@gmail.com>
2021-11-20 00:18:36 +0100

    Optimized Code

---
+++ readme.md
@@ -0,0 +1,9 @@
+## Plugin description
+
+**The plugin sets the border size to the level of the player(s).
+The border size is of course reloaded when a player's level increases, but also when a new player joins the world or when a player respawns.
+However, it is not a normal border, but a border that only applies to the player himself, which in turn means that monsters can spawn outside the border.
+The plugin can be played in single player but also in multiplayer mode. The starting size of the border is a 4 by 4 field. One level corresponds to approximately one block.
+The exact calculation of the size of the border is done like this:
+First, the total levels of all players are multiplied by 1.8. Then 3 is added to the result. This gives the total size of the border.
+This border is set using the WorldBorder API plugin.**
--- src/main/java/com/jannis/levelisborder/LevelIsBorder.java
+++ src/main/java/com/jannis/levelisborder/LevelIsBorder.java
@@ -1,6 +1,5 @@
-package com.spigot.jannis.levelisborder;
+package com.jannis.levelisborder;
 
-import com.github.yannicklamprecht.worldborder.api.BorderAPI;
 import com.github.yannicklamprecht.worldborder.api.WorldBorderApi;
 import org.bukkit.World;
 import org.bukkit.entity.Player;
@@ -11,20 +10,26 @@ import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerLevelChangeEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
-import org.bukkit.event.player.PlayerRespawnEvent;
 import org.bukkit.plugin.PluginManager;
+import org.bukkit.plugin.RegisteredServiceProvider;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import java.util.List;
 
+/**
+ *The border size corresponds to the level of all players.
+ * @author Jannis
+ */
 public final class LevelIsBorder extends JavaPlugin {
     private WorldBorderApi worldBorderApi;
 
     private Player latestplayer;
     private boolean reloadborder;
+    private int all_levels;
+
     public class JoinListener implements Listener {
         @EventHandler(priority = EventPriority.HIGHEST)
-        public void onEvent(PlayerJoinEvent event){
+        public void onEvent(PlayerJoinEvent event) {
             Player player = event.getPlayer();
             player.setLevel(player.getLevel() + 1);
             latestplayer = player;
@@ -34,7 +39,7 @@ public final class LevelIsBorder extends JavaPlugin {
 
     public class DeathListener implements Listener {
         @EventHandler(priority = EventPriority.HIGHEST)
-        public void onEvent(PlayerDeathEvent event){
+        public void onEvent(PlayerDeathEvent event) {
             Player player = event.getEntity();
             player.setLevel(player.getLevel() + 1);
             latestplayer = player;
@@ -49,9 +54,6 @@ public final class LevelIsBorder extends JavaPlugin {
             World world = player.getWorld();
             List<Player> players = world.getPlayers();
 
-            int playerlevel = player.getLevel();
-            int all_levels = 0;
-
             for (Player pl : players) {
                 all_levels = all_levels + pl.getLevel();
             }
@@ -64,8 +66,8 @@ public final class LevelIsBorder extends JavaPlugin {
 
     public class MoveListener implements Listener {
         @EventHandler(priority = EventPriority.NORMAL)
-        public void onEvent(PlayerMoveEvent event){
-            if (reloadborder && latestplayer.getUniqueId() == event.getPlayer().getUniqueId()){
+        public void onEvent(PlayerMoveEvent event) {
+            if (reloadborder && latestplayer.getUniqueId() == event.getPlayer().getUniqueId()) {
                 latestplayer.setLevel(latestplayer.getLevel() - 1);
                 reloadborder = false;
             }
@@ -88,7 +90,15 @@ public final class LevelIsBorder extends JavaPlugin {
         MoveListener moveListener = new MoveListener();
         pluginManager.registerEvents(moveListener, this);
 
-        worldBorderApi = BorderAPI.getApi();
+        RegisteredServiceProvider<WorldBorderApi> worldBorderApiRegisteredServiceProvider = getServer().getServicesManager().getRegistration(WorldBorderApi.class);
+
+        if (worldBorderApiRegisteredServiceProvider == null) {
+            getLogger().info("API not found");
+            getServer().getPluginManager().disablePlugin(this);
+            return;
+        }
+
+        worldBorderApi = worldBorderApiRegisteredServiceProvider.getProvider();
     }
 
     @Override
